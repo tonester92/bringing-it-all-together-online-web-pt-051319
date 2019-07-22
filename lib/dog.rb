7f9b33ea5753c9c523398a46d6f57488500637f9
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-22 01:08:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,14 @@ class Dog
     self.id ||= nil
   end
   
+  def self.create_table
+      sql = <<-SQL
+         CREATE TABLE IF NOT EXISTS dogs (
+         id INTEGER PRIMARY KEY,
+         name TEXT,
+         breed TEXT
+         )
+      SQL
+      DB[:conn].execute(sql)
+   end
   
