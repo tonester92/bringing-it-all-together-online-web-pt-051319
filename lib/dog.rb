e74aa92f76c6e4d96c1064e59bdc04db81c4e70a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-22 01:08:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 class Dog
   attr_accessor :id, :name, :breed
   
-  def initialize(id: nil, name:, breed:)
-      @id = id
-      @name = name
-      @breed = breed
-   end
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
