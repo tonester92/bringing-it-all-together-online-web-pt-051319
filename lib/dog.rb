56934184d345bcb577226df84aac3215ab03163f
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-22 01:20:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,9 +86,7 @@ class Dog
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
-  end
-
-  def self.find_or_create_by(name:, breed:)
+  enddef self.find_or_create_by(name:, breed:)
     sql = <<-SQL
       SELECT * 
       FROM dogs
